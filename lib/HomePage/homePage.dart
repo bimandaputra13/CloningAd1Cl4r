97f9e78a18c25b99f2d8a82d75c6cf445cd29ac1
bimandaputra13 <bimandaputra94@gmail.com>
2023-09-05 17:26:49 +0700

    First Commit clone PRJ

---
+++ lib/HomePage/homePage.dart
@@ -0,0 +1,256 @@
+import 'package:easy_sidemenu/easy_sidemenu.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Ad1clar',
+      theme: ThemeData(
+        primarySwatch: Colors.yellow,
+      ),
+      home: const MyHomePage(title: 'Selamat datang di Adira Finance'),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  PageController pageController = PageController();
+  SideMenuController sideMenu = SideMenuController();
+
+  @override
+  void initState() {
+    sideMenu.addListener((index) {
+      pageController.jumpToPage(index);
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title,style: TextStyle(color: const Color.fromARGB(255, 255, 255, 255),fontWeight: FontWeight.bold, fontFamily: 'Kanit', fontSize: 25)),
+        backgroundColor: const Color.fromARGB(255, 0, 0, 0),
+        centerTitle: true,
+      ),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+      
+        children: [
+          SideMenu(
+            controller: sideMenu,
+            style: SideMenuStyle(
+              // showTooltip: false,
+              displayMode: SideMenuDisplayMode.auto,
+              hoverColor: Color.fromARGB(255, 172, 172, 172),
+              selectedHoverColor: Color.fromARGB(255, 240, 240, 240),
+              selectedColor: Color.fromARGB(255, 42, 42, 41),
+              selectedTitleTextStyle: const TextStyle(color: Colors.white),
+              selectedIconColor: Colors.white,
+              backgroundColor: Colors.yellow[600],
+              // decoration: BoxDecoration(
+              //   borderRadius: BorderRadius.all(Radius.circular(10)),
+              // ),
+              // backgroundColor: Colors.blueGrey[700]
+            ),
+            title: Column(
+              children: [
+                ConstrainedBox(
+                  constraints: const BoxConstraints(
+                    maxHeight: 150,
+                    maxWidth: 150,
+                  ),
+                  child: Image.asset(
+                    'assets/images/adiraLogo.png',
+                  ),
+                ),
+                const Divider(
+                  indent: 8.0,
+                  endIndent: 8.0,
+                ),
+              ],
+            ),
+            footer: Padding(
+              padding: const EdgeInsets.all(8.0),
+               child: Container(
+              //   decoration: BoxDecoration(
+              //       color: Color.fromARGB(255, 49, 67, 75),
+              //       borderRadius: BorderRadius.circular(12)),
+              //   child: Padding(
+              //     padding:
+              //         const EdgeInsets.symmetric(vertical: 4, horizontal: 10),
+              //     child: Text(
+              //       'mohada',
+              //       style: TextStyle(fontSize: 15, color: Colors.grey[800]),
+              //     ),
+              //   ),
+              ),
+            ),
+            items: [
+              SideMenuItem(
+                title: 'Home',
+                onTap: (index, _) {
+                  sideMenu.changePage(index);
+                },
+                icon: const Icon(Icons.home),
+                //  badgeContent: const Text(
+                //    '3',
+                //    style: TextStyle(color: Colors.white),
+                //  ),
+               // tooltipContent: "This is a tooltip for Dashboard item",
+              ),
+              SideMenuItem(
+                title: 'Management Commercial Fleet',
+                onTap: (index, _) {
+                  sideMenu.changePage(index);
+                },
+                icon: const Icon(Icons.menu_book_rounded),
+              ),
+              SideMenuItem(
+                title: 'Manajemen Sistem',
+                onTap: (index, _) {
+                  sideMenu.changePage(index);
+                  
+                },
+                icon: const Icon(Icons.account_circle_sharp),
+              ),
+              SideMenuItem(
+                title: 'Manajemen Collection',
+                onTap: (index, _) {
+                  sideMenu.changePage(index);
+                },
+                icon: const Icon(Icons.collections),
+              ),
+              SideMenuItem(
+                builder: (context, displayMode) {
+                  return const Divider(
+                    endIndent: 8,
+                    indent: 8,
+                  );
+                },
+              ),
+              SideMenuItem(
+                title: 'Manajemen Recovery',
+                onTap: (index, _) {
+                  sideMenu.changePage(index);
+                },
+                icon: const Icon(Icons.restart_alt_rounded),
+              ),
+              // SideMenuItem(
+              //   onTap:(index, _){
+              //     sideMenu.changePage(index);
+              //   },
+              //   icon: const Icon(Icons.image_rounded),
+              // ),
+              // SideMenuItem(
+              //   title: 'Only Title',
+              //   onTap:(index, _){
+              //     sideMenu.changePage(index);
+              //   },
+              // ),
+              const SideMenuItem(
+                title: 'Manajemen Warehouse',
+                icon: Icon(Icons.warehouse),
+              ),
+              const SideMenuItem(
+                title: 'Riwayat Pembayaran',
+                icon: Icon(Icons.history),
+              ),
+              const SideMenuItem(
+                title: 'FAQ',
+                icon: Icon(Icons.menu_rounded),
+              ),
+            ],
+          ),
+          Expanded(
+            child: PageView(
+              controller: pageController,
+              children: [
+                Container(
+                  color: Colors.white,
+                  child: const Center(
+                    child: Text(
+                      'Home',
+                      style: TextStyle(fontSize: 35),
+                    ),
+                  ),
+                ),
+                Container(
+                  color: Colors.white,
+                  child: const Center(
+                    child: Text(
+                      'Users',
+                      style: TextStyle(fontSize: 35),
+                    ),
+                  ),
+                ),
+                Container(
+                  color: Colors.white,
+                  child: const Center(
+                    child: Text(
+                      'Files',
+                      style: TextStyle(fontSize: 35),
+                    ),
+                  ),
+                ),
+                Container(
+                  color: Colors.white,
+                  child: const Center(
+                    child: Text(
+                      'Download',
+                      style: TextStyle(fontSize: 35),
+                    ),
+                  ),
+                ),
+                Container(
+                  color: Colors.white,
+                  child: const Center(
+                    child: Text(
+                      'Settings',
+                      style: TextStyle(fontSize: 35),
+                    ),
+                  ),
+                ),
+                Container(
+                  color: Colors.white,
+                  child: const Center(
+                    child: Text(
+                      'Only Title',
+                      style: TextStyle(fontSize: 35),
+                    ),
+                  ),
+                ),
+                Container(
+                  color: Colors.white,
+                  child: const Center(
+                    child: Text(
+                      'Only Icon',
+                      style: TextStyle(fontSize: 35),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,308 @@
+import 'package:flutter/material.dart';
+import 'HomePage/homePage.dart';
+
+void main() => runApp(const SignUpApp());
+
+class SignUpApp extends StatelessWidget {
+  const SignUpApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      routes: {
+        '/': (context) => const SignUpScreen(),
+        '/tabBar': (context) => const MyHomePage(
+              title: 'Selamat datang di Adira Finance',
+            ),
+      },
+    );
+  }
+}
+
+class SignUpScreen extends StatelessWidget {
+  const SignUpScreen();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/bg-adira.jpg"),
+            fit: BoxFit.fill,
+          ),
+        ),
+        child: const Center(
+          child: SizedBox(
+            width: 400,
+            child: Card(
+              //color: Color.fromARGB(255, 151, 151, 151),
+              child: SignUpForm(),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SignUpForm extends StatefulWidget {
+  const SignUpForm();
+
+  @override
+  State<SignUpForm> createState() => _SignUpFormState();
+}
+
+class _SignUpFormState extends State<SignUpForm> {
+  final _usernameTxt = TextEditingController();
+  final _passwordTxt = TextEditingController();
+
+  double _formProgress = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      //onChanged: _updateFormProgress,
+      child: SizedBox(
+        height: 200,
+        child: Stack(
+          children: [
+            SingleChildScrollView(
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 20),
+                    child: Column(
+                      children: [
+                        _usernameWidget(),
+                        _passwordWidget(),
+                        SizedBox(height: 20),
+                        Center(
+                          child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                _submitButton(context),
+                                _clearButton(context),
+                              ]),
+                        )
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Container(
+                    child: Column(children: [
+                      TextButton(
+                        style: ButtonStyle(
+                          foregroundColor: MaterialStateProperty.resolveWith(
+                              (Set<MaterialState> states) {
+                            return states.contains(MaterialState.disabled)
+                                ? null
+                                : Color.fromARGB(255, 255, 252, 252);
+                          }),
+                          backgroundColor: MaterialStateProperty.resolveWith(
+                              (Set<MaterialState> states) {
+                            return states.contains(MaterialState.disabled)
+                                ? null
+                                : Color.fromARGB(255, 255, 255, 255);
+                          }),
+                        
+                        ),
+                        //onPressed: _formProgress == 1 ?  _showWelcomeScreen: null,
+                        onPressed: () {
+                          Navigator.pushNamed(context, '/tabBar');
+                          //_formProgress == 1 ?  _showWelcomeScreen: null;
+                        },
+                        child: const Text('Lupa Password ???',style: TextStyle(fontSize: 9, color: Colors.black),),
+                      ),
+                    ]),
+                  ),
+                ],
+              ),
+            ),
+            //Positioned(top: 60, left: 0, child: _backButton()),
+          ],
+        ),
+
+        // Text('CLAR Login - Version 4.8.0',
+        //     style: TextStyle(
+        //         height: 5, fontSize: 12, fontWeight: FontWeight.bold, color: Colors.white)),
+        // Padding(
+        //   padding: const EdgeInsets.all(8),
+
+        //   child: Card(
+        //     child: TextField(
+        //       controller: _usernameTxt,
+        //       decoration: const InputDecoration(
+        //          hintText: 'NIK',
+        //         //  constraints: const BoxConstraints(
+        //         //   maxHeight: 150,
+        //         //   maxWidth: 150,
+        //         // ),
+        //        contentPadding:
+        //            const EdgeInsets.only(left: 14.0, bottom: 8.0, top: 8.0),
+        //        focusedBorder: OutlineInputBorder(
+        //          borderSide: BorderSide(color: Color.fromARGB(255, 175, 175, 175)),
+        //        ),
+
+        //       ),
+        //     ),
+
+        //   ) ,
+        // ),
+        // Padding(
+        //   padding: const EdgeInsets.all(8),
+        //   child : Card(
+        //   child: TextField(
+        //     obscureText: true,
+        //     controller: _passwordTxt,
+        //     decoration: const InputDecoration(
+        //       hintText: 'Password',
+        //       contentPadding:
+        //           const EdgeInsets.only(left: 14.0, bottom: 8.0, top: 8.0),
+        //       focusedBorder: OutlineInputBorder(
+        //         borderSide: BorderSide(color: Color.fromARGB(255, 175, 175, 175)),
+        //       ),
+        //       enabledBorder: UnderlineInputBorder(
+        //         borderSide: BorderSide(color: Colors.white),
+        //       ),
+
+        //     ),
+        //   ),
+        //   ),
+        // ),
+      ),
+    );
+  }
+
+  void _updateFormProgress() {
+    var progress = 0.0;
+    final controllers = [
+      _usernameTxt,
+      _usernameTxt,
+    ];
+
+    for (final controller in controllers) {
+      if (controller.value.text.isNotEmpty) {
+        progress += 1 / controllers.length;
+      }
+    }
+
+    setState(() {
+      _formProgress = progress;
+    });
+  }
+}
+
+Widget _submitButton(BuildContext context) {
+  return Align(
+    alignment: Alignment.center,
+    child: InkWell(
+      // onTap: () {
+      //   // Navigator.push(
+      //   //     context, MaterialPageRoute(builder: (context) => SignUpPage()));
+      // },
+      child:
+          //Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+          ElevatedButton(
+        style: ButtonStyle(
+          foregroundColor:
+              MaterialStateProperty.resolveWith((Set<MaterialState> states) {
+            return states.contains(MaterialState.disabled)
+                ? null
+                : Color.fromARGB(255, 255, 252, 252);
+          }),
+          backgroundColor:
+              MaterialStateProperty.resolveWith((Set<MaterialState> states) {
+            return states.contains(MaterialState.disabled)
+                ? null
+                : Color.fromARGB(255, 65, 65, 66);
+          }),
+          alignment: Alignment.center,
+        ),
+        //onPressed: _formProgress == 1 ?  _showWelcomeScreen: null,
+        onPressed: () {
+          Navigator.pushNamed(context, '/tabBar');
+          //_formProgress == 1 ?  _showWelcomeScreen: null;
+        },
+        child: const Text('Login'),
+      ),
+    ),
+  );
+}
+
+Widget _clearButton(BuildContext context) {
+  return Align(
+    alignment: Alignment.center,
+    child: InkWell(
+      // onTap: () {
+      //   // Navigator.push(
+      //   //     context, MaterialPageRoute(builder: (context) => SignUpPage()));
+      // },
+      child:
+          //Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+          ElevatedButton(
+        style: ButtonStyle(
+          foregroundColor:
+              MaterialStateProperty.resolveWith((Set<MaterialState> states) {
+            return states.contains(MaterialState.disabled)
+                ? null
+                : Color.fromARGB(255, 255, 252, 252);
+          }),
+          backgroundColor:
+              MaterialStateProperty.resolveWith((Set<MaterialState> states) {
+            return states.contains(MaterialState.disabled)
+                ? null
+                : Color.fromARGB(255, 65, 65, 66);
+          }),
+          alignment: Alignment.center,
+        ),
+        //onPressed: _formProgress == 1 ?  _showWelcomeScreen: null,
+        onPressed: () {
+          Navigator.pushNamed(context, '/tabBar');
+          //_formProgress == 1 ?  _showWelcomeScreen: null;
+        },
+        child: const Text('Clear'),
+      ),
+    ),
+  );
+}
+
+Widget _usernameWidget() {
+  return Stack(
+    children: [
+      TextFormField(
+        keyboardType: TextInputType.name,
+        textInputAction: TextInputAction.next,
+        decoration: InputDecoration(
+          labelText: 'NIK',
+          labelStyle: TextStyle(
+              color: Color.fromRGBO(172, 172, 172, 1),
+              fontWeight: FontWeight.bold),
+          enabledBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: Color.fromRGBO(173, 183, 192, 1)),
+          ),
+        ),
+      ),
+    ],
+  );
+}
+
+Widget _passwordWidget() {
+  return Stack(
+    children: [
+      TextFormField(
+        keyboardType: TextInputType.name,
+        textInputAction: TextInputAction.next,
+        decoration: InputDecoration(
+          labelText: 'Password',
+          labelStyle: TextStyle(
+              color: Color.fromRGBO(172, 172, 172, 1),
+              fontWeight: FontWeight.bold),
+          enabledBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: Color.fromRGBO(173, 183, 192, 1)),
+          ),
+        ),
+      ),
+    ],
+  );
+}
